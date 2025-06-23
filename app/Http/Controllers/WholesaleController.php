aa23069e45946d1d4f7b3beb9988be9871674998
Azamov Samandar <JscorpTech@gmail.com>
2025-06-23 14:00:51 +0200

    fix typo

--- app/Http/Controllers/WholesaleController.php
+++ app/Http/Controllers/WholesaleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Events\WholesaleEvent;
 use App\Models\Product;
 use App\Models\User;
+use App\Models\Seller;
 use Illuminate\Http\Request;
 
 class WholesaleController extends Controller
@@ -15,8 +16,7 @@ class WholesaleController extends Controller
             "product_id" => ["required", "exists:products,id"],
         ]);
         $product = Product::find($validated_data["product_id"]);
-        $seller = User::find($product->user_id);
-        dd($seller);
+        $seller = Seller::find($product->user_id);
         $data = [
             'subject' => translate('new_order_received'),
             'title' => translate('new_order_received'),
