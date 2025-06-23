c1f73cf890c8e8f5a9eb5fc14738a305c648a6f5
A'zamov Samandar <admin@jscorp.uz>
2025-06-23 18:41:28 +0500

    fix typo

--- resources/views/admin-views/business-settings/email-template/vendor-mail-template/wholesale.blade.php
+++ resources/views/admin-views/business-settings/email-template/vendor-mail-template/wholesale.blade.php
@@ -1,17 +1,17 @@
 <div class="p-3 px-xl-4 py-sm-5">
     <h3 class="mb-4 view-mail-title text-capitalize">
-        {{$title}}
+        {{$title ?? 'Default Title'}}
     </h3>
     <div class="view-mail-body">
-        {!! $body !!}
+        {!! $body ?? 'Default body content' !!}
     </div>
-    <p>{{translate('Customer')}}: {{$data['user']['f_name'].' '.$data['user']['l_name'] ?? 'John Doe'}}</p>
+    <p>{{translate('Customer')}}: {{($data['user']['f_name'] ?? 'John').' '.($data['user']['l_name'] ?? 'Doe')}}</p>
     <p>{{translate('Email')}}: {{$data['user']['email'] ?? 'example@gmail.com'}}</p>
     <p>{{translate('Phone')}}: {{$data['user']['phone'] ?? '123-456-7890'}}</p>
     <p>{{translate('product')}}:</p>
     <ul>
-        @foreach($data['products'] ?? [['name'=>'Product 1', 'variant'=>'Variant 1']] as $product)
-            <li>{{$product['name'].' - '.$product['variant']}}</li>
+        @foreach($data['products'] ?? [['name' => 'Product 1', 'variant' => 'Variant 1']] as $product)
+            <li>{{($product['name'] ?? 'Default Product').' - '.($product['variant'] ?? 'Default Variant')}}</li>
         @endforeach
     </ul>
     @include('admin-views.business-settings.email-template.partials-design.footer-design-without-logo')
