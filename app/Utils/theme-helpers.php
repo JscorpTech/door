a828c4d9090e0d977a05ca2e009e9513d939aab3
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-08-06 17:37:02 +0500

    New g4f version: 0.5.8.1 (current: 0.3.9.7) | pip install -U g4f

--- app/Utils/theme-helpers.php
+++ app/Utils/theme-helpers.php
@@ -3,18 +3,16 @@
 if (!function_exists('theme_asset')) {
     function theme_asset($path = null): string
     {
-        $themeName = theme_root_path();
-        if($themeName == 'default'){
-            return dynamicAsset(path: $path);
-        }else{
-            if (DOMAIN_POINTED_DIRECTORY == 'public') {
-                return dynamicAsset(path: 'public/themes/'.$themeName.'/public/'.$path);
-            }else{
-                return dynamicAsset(path: 'resources/themes/'.$themeName.'/public/'.$path);
-            }
+        $themeName = 'theme_aster'; // Har doim shu bo'ladi
+
+        if (DOMAIN_POINTED_DIRECTORY == 'public') {
+            return dynamicAsset(path: 'public/themes/' . $themeName . '/public/' . $path);
+        } else {
+            return dynamicAsset(path: 'resources/themes/' . $themeName . '/public/' . $path);
         }
     }
 }
+
 
 if (!function_exists('theme_root_path')) {
     function theme_root_path(): string
