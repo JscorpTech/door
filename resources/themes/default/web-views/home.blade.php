188b63922f63ea4ab0e41fe279efcd4dc040b7f0
husanjon <husanjon@felixits.uz>
2025-11-04 14:53:48 +0500

    New g4f version: 6.5.7 (current: 0.3.9.7) | pip install -U g4f

--- resources/themes/default/web-views/home.blade.php
+++ resources/themes/default/web-views/home.blade.php
@@ -31,7 +31,7 @@
             <div class="container py-4 rtl px-0 px-md-3">
                 <div class=" pt-3">  
                     <h2 class="feature-product-title mt-0 web-text-primary mb-0 letter-spacing-0">
-                        {{ translate('featured_ products') }}
+                        {{ translate('featured_products') }}
                     </h2>
                     <div class="text-end px-3 d-none d-md-block">
                         <a class="text-capitalize view-all-text web-text-primary" href="{{route('products',['data_from'=>'featured','page'=>1])}}">
